aceee9cb8c075c4d8d5ec5ae8035d84e5acd0618
haseebrehmanpc <haseeb@geeksofkolachi.com>
2024-01-08 16:13:22 +0500

    keeping aevo alive

--- websocket/Aevo/index.js
+++ websocket/Aevo/index.js
@@ -7,6 +7,17 @@ wsAevo.on("error", (err) => socketError("wsAevo", err));
 wsAevo.on("open", function open() {
   console.log("opened connection for wsAevo");
 });
+function keepAevoLive() {
+  wsAevo.send(
+    JSON.stringify({
+      id: 1,
+      op: "ping",
+    })
+  );
+  console.log("Keeping Aevo alive!");
+}
+
+const intervalId = setInterval(keepAevoLive, 15 * 60 * 1000);
 
 module.exports = {
   wsAevo,
